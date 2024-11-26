75cf7449ee02e3bd05bf705370648ab33238ad15
Rohit Dogra <164403018+Rohitdogra09@users.noreply.github.com>
2024-11-26 13:28:48 +0100

    Add files via upload

--- lanes.py
+++ lanes.py
@@ -33,7 +33,7 @@ def average_slope_intercept(image, lines):
 
 
 def canny(image):
-    gray=cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray=cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5),0)
     canny = cv2.Canny(blur, 50,150)
     return canny
@@ -55,7 +55,7 @@ def region_of_interest(image):
     masked_image=cv2.bitwise_and(image, mask) #
     return masked_image
 
-image = cv2.imread('test_image.jpg')
+'''image = cv2.imread('test_image.jpg')
 lane_image=np.copy(image)
 canny_image=canny(lane_image)
 cropped_image = region_of_interest(canny_image)
@@ -64,4 +64,16 @@ averaged_lines= average_slope_intercept(lane_image, lines)
 line_image=display_lines(lane_image, averaged_lines)
 combo_image=cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow("result", combo_image)
-cv2.waitKey(0)
+cv2.waitKey(0)'''
+
+cap=cv2.VideoCapture("test2.mp4")
+while(cap.isOpened()):
+    _, frame=cap.read()
+    canny_image=canny(frame)
+    cropped_image = region_of_interest(canny_image)
+    lines=cv2.HoughLinesP(cropped_image,2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines= average_slope_intercept(frame, lines)
+    line_image=display_lines(frame, averaged_lines)
+    combo_image=cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow("result", combo_image)
+    cv2.waitKey(1)
